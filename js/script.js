204b615e364f24217bdf8b9620610063751cfef4
DavideAnile <davide.anile16@gmail.com>
2023-03-14 16:31:01 +0100

    feat : bonus 1 added

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@
 
 Descrizione:
 Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
+
+
 Bonus:
-al click su una thumb, visualizzare in grande l'immagine corrispondente
-applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
-quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 Consigli del giorno:
 - regola d'oro: riciclare ovunque possibile! Questo significa che per la parte di markup possiamo recuperare html 
 e css dell'esercizio svolto qualche giorno fa: è già tutto pronto!
@@ -86,7 +88,9 @@ const { createApp } = Vue
                 this.currentIndex--
             }
 
-        }
+        },
+
+       
     }
 
   }).mount('#app')
